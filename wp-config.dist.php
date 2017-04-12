a785a4e409cd084881267ee3d3e12bd40acdd6dd
Marko Mitranic <marko.mitranic@catenamedia.com>
2017-04-12 22:43:41 +0200

    Fix a file that was defected with a simple copy paste from ssh nano

--- wp-config.dist.php
+++ wp-config.dist.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database');
+define('DB_NAME', 'DATABASE');
 
 /** MySQL database username */
-define('DB_USER', 'user');
+define('DB_USER', 'USERNAME');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password');
+define('DB_PASSWORD', 'PASSWORD');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -37,19 +37,19 @@ define('DB_COLLATE', '');
  * Authentication Unique Keys and Salts.
  *
  * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ W$
- * You can change these at any point in time to invalidate all existing cookies. This will $
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'h9/Q_3V3JD0[>A_Z!BIQ/y$.T0Ds398oY334DpV^J)/,=%7THt.NK9VNcrGV$1|$
-define('SECURE_AUTH_KEY',  'P5S^}P7ZteC<,KSBg%U$vfTnhA6uN}<,]m7DNb5gJx+)K|EZ)P0Q^*FJr|ww`J4$
-define('LOGGED_IN_KEY',    'zbZCY2yBw*<(@sD OJY 3f2|ts-`b}r.F@QI26OUJ>94**5!H*V]X,MPm(jx!a8$
-define('NONCE_KEY',        'hFuDsY^=fsyyrAx|>=D$LXHt>-4@2vg#qW(1?@udGW9:uoP`La<Q8T_JH<1Q?5-$
-define('AUTH_SALT',        '}]Tnw$b_E$%i?ej-9qwic.gl|nyC2.SHrWDq>5xi+W+Yn=(hm27hsry[,GitFvN$
-define('SECURE_AUTH_SALT', 'j[Q9yI?|CO#A11~mB)vrG+/BXgze5A@cUDp-+m$r_))`-XKKy&/dK&v|b^F%375$
-define('LOGGED_IN_SALT',   '),O0HZy?+j+/12,!-!-,79rd4x7H4&q-i^|yI~bD-`36)Ya=*mOpU~jA@SE;jG2$
-define('NONCE_SALT',       '~E{*(r{(;/gq?Eu/M6;Xka8hHDU--TuYRw;(_HU;JV9.T1qf+tz?#p;R~N?94k]$
+define('AUTH_KEY',         'h9/Q_3V3JD0[>A_Z!BIQ/y$.T0Ds398oY334DpV^J)/,=%7THt.NK9VNcrGV$1|P');
+define('SECURE_AUTH_KEY',  'P5S^}P7ZteC<,KSBg%U$vfTnhA6uN}<,]m7DNb5gJx+)K|EZ)P0Q^*FJr|ww`J4{');
+define('LOGGED_IN_KEY',    'zbZCY2yBw*<(@sD OJY 3f2|ts-`b}r.F@QI26OUJ>94**5!H*V]X,MPm(jx!a8$');
+define('NONCE_KEY',        'hFuDsY^=fsyyrAx|>=D$LXHt>-4@2vg#qW(1?@udGW9:uoP`La<Q8T_JH<1Q?5-S');
+define('AUTH_SALT',        '}]Tnw$b_E$%i?ej-9qwic.gl|nyC2.SHrWDq>5xi+W+Yn=(hm27hsry[,GitFvN8');
+define('SECURE_AUTH_SALT', 'j[Q9yI?|CO#A11~mB)vrG+/BXgze5A@cUDp-+m$r_))`-XKKy&/dK&v|b^F%375Z');
+define('LOGGED_IN_SALT',   '),O0HZy?+j+/12,!-!-,79rd4x7H4&q-i^|yI~bD-`36)Ya=*mOpU~jA@SE;jG2/');
+define('NONCE_SALT',       '~E{*(r{(;/gq?Eu/M6;Xka8hHDU--TuYRw;(_HU;JV9.T1qf+tz?#p;R~N?94k]l');
 
 /**#@-*/
 
@@ -84,8 +84,7 @@ define('WP_DEBUG', false);
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
-        define('ABSPATH', dirname(__FILE__) . '/');
+    define('ABSPATH', dirname(__FILE__) . '/');
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
-
